e9127557fd8c7482816d1b1207befcd4a812c49e
Alexander Kachan <akach.dev@mail.ru>
2023-12-22 17:36:06 +0300

    case Ошибки / addMatcher / matching utilities  done

---
+++ src/features/auth/model/authSlice.ts
@@ -0,0 +1,59 @@
+import { createSlice, isFulfilled, PayloadAction } from "@reduxjs/toolkit";
+import { authAPI, LoginParamsType } from "features/auth/api/auth.api";
+import { clearTasksAndTodolists } from "common/actions";
+import { createAppAsyncThunk } from "common/utils";
+import { ResultCode } from "common/enums";
+
+const login = createAppAsyncThunk<{ isLoggedIn: boolean }, LoginParamsType>(
+  "auth/login",
+  async (arg, { rejectWithValue }) => {
+    const res = await authAPI.login(arg);
+    if (res.data.resultCode === ResultCode.Success) {
+      return { isLoggedIn: true };
+    } else {
+      return rejectWithValue(res.data);
+    }
+  },
+);
+
+const logout = createAppAsyncThunk<{ isLoggedIn: boolean }, void>("auth/logout", async (_, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI;
+  const res = await authAPI.logout();
+  if (res.data.resultCode === ResultCode.Success) {
+    dispatch(clearTasksAndTodolists());
+    return { isLoggedIn: false };
+  } else {
+    return rejectWithValue(res.data);
+  }
+});
+
+const initializeApp = createAppAsyncThunk<{ isLoggedIn: boolean }, void>(
+  "app/initializeApp",
+  async (_, { rejectWithValue }) => {
+    const res = await authAPI.me();
+    if (res.data.resultCode === ResultCode.Success) {
+      return { isLoggedIn: true };
+    } else {
+      return rejectWithValue(res.data);
+    }
+  },
+);
+
+const slice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      isFulfilled(initializeApp, logout, login),
+      (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
+        state.isLoggedIn = action.payload.isLoggedIn;
+      },
+    );
+  },
+});
+
+export const authSlice = slice.reducer;
+export const authThunks = { login, logout, initializeApp };
